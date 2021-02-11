797db5e910a836f422e02b0f0cb42aa40b5037c8
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-02-11 07:54:01 +0000

    added time

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const title = <h1>React element.</h1>;
+const title = <h1>Scoreboard</h1>;
 
-const desc = <p>This is a paragraph.</p>;
+// const desc = <p>This is a paragraph.</p>;
+
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+
+    
+    const desc = <p><span>{hour}</span><span> : </span> <span>{minutes}</span></p>;
+
+    
+
+// const desc = <p><span>{hour}</span><span> : </span> <span>{minutes}</span></p>;
 
 const header = React.createElement(
     'header',
